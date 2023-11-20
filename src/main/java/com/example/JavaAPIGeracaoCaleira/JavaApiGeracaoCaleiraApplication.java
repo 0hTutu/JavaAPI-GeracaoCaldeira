19f929f0fde088d9882779e38fde76d965e10d31
0hTutu <arthurmullercavalheiro@gmail.com>
2023-11-20 05:22:50 -0300

    Criação do projeto Java com Spring.

---
+++ src/main/java/com/example/JavaAPIGeracaoCaleira/JavaApiGeracaoCaleiraApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaAPIGeracaoCaleira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiGeracaoCaleiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiGeracaoCaleiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/JavaAPIGeracaoCaleira/JavaApiGeracaoCaleiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaAPIGeracaoCaleira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiGeracaoCaleiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
